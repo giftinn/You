a9feffcf3343621d7181db59ef3a83566c6f69b2
giftin.id <calistanafisaa@gmail.com>
2026-04-09 23:14:48 +0700

    Update script.js

--- script.js
+++ script.js
@@ -74,7 +74,7 @@ I don’t even know where to start this, but please let me tell you how much I m
 
 We should have been six months by now, right? It hurts knowing we can’t celebrate that together anymore. All I can do is hold onto the memories we made.
 
-Thank you for once being the best part of my days. Honestly, I can’t hold it in anymore — I miss you so much. Every day feels so quiet. There are no notifications from you anymore, even though they used to be the thing I looked forward to the most.
+Thank you for once being the best part of my days. Honestly, I can’t hold it in anymore.. I miss you so much. Every day feels so quiet. There are no notifications from you anymore, even though they used to be the thing I looked forward to the most.
 
 I know we probably can’t be together again. I understand that. But please let me express how much I miss you through this. Maybe you don’t like it, and maybe it makes things complicated… but at least I hope you can accept this small, simple gift from me.
 thank you for opened this gift, i still love you. 
